060718dd6bff9281aca2d5b5810cac053fbaf014
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-29 09:17:35 -0500

    set up day.router.js server side

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const promptRouter = require('./routes/prompt.router');
+const dayRouter = require('./routes/day.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -26,6 +27,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/prompt', promptRouter);
+app.use('/api/day', dayRouter);
 
 // Serve static files
 app.use(express.static('build'));
