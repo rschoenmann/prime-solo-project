fb5016f8b7444d0049f7208bf75401b65db08b7e
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-30 14:05:22 -0500

    changed Dashboard day rendering so checkboxes and Rating are readonly

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Checkbox, TextField, FormControl, FormControlLabel, FormLabel, FormGroup, FormHelperText, Button} from '@material-ui/core';
+import {Checkbox} from '@material-ui/core';
+import {Star, StarBorder} from '@material-ui/icons';
+import Rating from 'react-rating';
 
 class Dashboard extends Component {
 
@@ -16,20 +18,24 @@ class Dashboard extends Component {
 
 				{this.props.day.map((aDay) => {
 					return(
-						<FormControlLabel key={aDay.id}
-							control={<Checkbox value={aDay.id}
-								checked={aDay.prompt1} color="primary" />}
-							label={aDay.prompt1} />
-						// <div key={aDay.id}>
-						// 	<p>Date: {aDay.date}</p>
-						// 	<p>{aDay.prompt1}</p>
-						// 	<p>{aDay.prompt2}</p>
-						// 	<p>{aDay.prompt3}</p>
-						// 	<p>{aDay.prompt4}</p>
-						// 	<p>{aDay.prompt5}</p>
-						// 	<p>{aDay.rating}</p>
-						// 	<p>{aDay.notes}</p>
-						// </div>
+						<div key={aDay.id}>
+							<p>Date: {aDay.date}</p>
+							<p>Did you get enough sleep last night <Checkbox value={aDay.prompt1}
+								checked={aDay.prompt1} color="primary" disabled/></p>
+							<p>Did you drink enough water today? <Checkbox value={aDay.prompt2}
+								checked={aDay.prompt2} color="primary" disabled/></p>
+							<p>Did you go outside today? <Checkbox value={aDay.prompt3}
+								checked={aDay.prompt3} color="primary" disabled/></p>
+							<p>Did you take your meds today? <Checkbox value={aDay.prompt4}
+								checked={aDay.prompt4} color="primary" disabled/></p>
+							<p>Did you talk to a friend today? <Checkbox value={aDay.prompt5}
+								checked={aDay.prompt5} color="primary" disabled/></p>
+							<p>Rating: <Rating initialRating={aDay.rating}
+								emptySymbol={<StarBorder />}
+								fullSymbol={<Star />}
+								start={0} stop={5} readonly /></p>
+							<p>Notes: {aDay.notes}</p>
+						</div>
 					)
 				})}
 			</div>
