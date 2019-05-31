1219e7223eda0d7cfb87a64f73ce2970cc659674
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-31 10:15:30 -0500

    DAY ROUTER ASYNC TRANSACTION POST WORKS FINALLY

--- server/routes/day.router.js
+++ server/routes/day.router.js
@@ -15,29 +15,41 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 	});
 });//end GET
 
-router.post('/', rejectUnauthenticated, (req, res) => {
+router.post('/', rejectUnauthenticated, async (req, res) => {
 	console.log('Is authenticated?', req.isAuthenticated());
 	console.log('req.body:', req.body);
 	console.log('req.user:', req.user);
-	const queryText = `INSERT INTO "review" ("user_id", "rating", "notes") 
-						VALUES ($1, $2, $3) RETURNING id;`;
-	const queryValues = [req.user.id,  req.body.value, req.body.notes]
-	pool.query(queryText, queryValues).then((result) => {
-		console.log('result.rows:', result.rows)
-		const queryText2 = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer")
-							VALUES ($1, $2, $3);`
-		const queryValues2 = [(1, result.rows[0].id, req.body['1']),
-			(2, result.rows[0].id, req.body['2']),
-			(3, result.rows[0].id, req.body['3']),
-			(4, result.rows[0].id, req.body['4']),
-			(5, result.rows[0].id, req.body['5'])]
-		pool.query(queryText2, queryValues2).then((result) => {
-			console.log('result.row2:', result.rows);
-			res.sendStatus(201);
-		})
-	}).catch(() => res.sendStatus(500));
+	//need to use the same connection for all queries
+	const connection = await pool.connect()
+	try{
+		await connection.query('BEGIN');
+		const addReview = `INSERT INTO "review" ("user_id", "rating", "notes") 
+						VALUES ($1, $2, $3) RETURNING id;`; //save the result so we can get the returned value
+		const addReviewValues = [req.user.id, req.body.value, req.body.notes]
+		const result = await connection.query(addReview, addReviewValues)
+		//save id of the current review we're inserting into to use in subsequent inserts
+		const reviewId = result.rows[0].id;
+		//each of the 5 prompts inserted into prompt_review separately
+		const promptOne = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer") VALUES ($1, $2, $3);`;
+		await connection.query(promptOne, [1, reviewId, req.body['1']]);
+		const promptTwo = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer") VALUES ($1, $2, $3);`;
+		await connection.query(promptTwo, [2, reviewId, req.body['2']]);
+		const promptThree = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer") VALUES ($1, $2, $3);`;
+		await connection.query(promptThree, [3, reviewId, req.body['3']]);
+		const promptFour = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer") VALUES ($1, $2, $3);`;
+		await connection.query(promptFour, [4, reviewId, req.body['4']]);
+		const promptFive = `INSERT INTO "prompt_review" ("prompt_id", "review_id", "answer") VALUES ($1, $2, $3);`;
+		await connection.query(promptFive, [5, reviewId, req.body['5']]);
+		await connection.query('COMMIT');
+		res.sendStatus(200);
+	}catch(error){
+		//if any of the above steps fail, abort the entire transaction so no bad info gets into database
+		await connection.query('ROLLBACK');
+		console.log('Transaction error - rolling back review entry:', error);
+		res.sendStatus(500);
+	}finally{
+		connection.release()
+	}
 });
 
 module.exports = router;
-
-// req.body['1'], req.body['2'], req.body['3'], req.body['4'], req.body['5'],
