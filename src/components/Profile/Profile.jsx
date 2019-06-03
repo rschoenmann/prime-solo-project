81578bb12131b1d140863aaeb79d170a44e04e88
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-06-03 13:12:46 -0500

    Profile now conditionally renders based on isEditable, added sweetalert on save changes button

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {InputAdornment, Button, FormControl, InputLabel, TextField, MenuItem, Select, OutlinedInput} from '@material-ui/core';
+import {Button} from '@material-ui/core';
+import Swal from 'sweetalert2';
 
 class Profile extends Component {
 
-	state ={
+	state = {
 		profileEditable: false,
 		username: this.props.user.username,
 		name: this.props.user.name,
-		gradient_id: this.props.user.gradient_id
+		gradient_id: 1
 	}
 
 	editProfile = () => {
@@ -25,56 +26,62 @@ class Profile extends Component {
 		});
 	};//end handleInputChange
 
+	saveChanges = () =>{
+		console.log('saveChanges');
+		this.setState({
+			profileEditable: false
+		});
+		Swal.fire({
+			type: 'success',
+			title: 'Thanks!',
+			text: 'Your profile has been updated',
+		});
+		
+	};//end saveChanges
+
 	render(){
+		console.log('is profile editable?', this.state.profileEditable)
 		return(
 			<div>
-				<h2>{this.props.user.name}'s Profile
+				<h2>{this.props.user.name}'s Profile</h2>
 						{/* ternary statement to conditionally render page based on if user is editing profile or not */}
 					    {this.state.profileEditable ?
-						<> <input type="text" placeholder="enter username" onChange={this.handleChange} /><br></br><button onClick={this.saveUsername}>Save Username</button></> :
+						<><form>
+							<label htmlFor="username">
+              				Username:
+              					<input type="text" name="username"
+                				value={this.state.username}
+                				onChange={this.handleInputChange('username')}/></label>
+							<br></br>
+
+							<label htmlFor="name">
+              				What should we call you?:
+              					<input type="text" name="name"
+                				value={this.state.name}
+								onChange={this.handleInputChange('name')} /></label>
+							<br></br>
+
+							
+							<label htmlFor="gradient_id">
+							Default color gradient:
+								<select value={this.props.user.gradient_id} onChange={this.handleInputChange('gradient_id')}>
+									<option value={0}>No preference</option>
+									<option value={1} >Red to Blue</option>
+									<option value={2} >Blue to Green</option>
+									<option value={3} >Yellow to Purple</option>
+								</select></label>
+							<br></br>
+
+							<p>GRADIENT EXAMPLE HERE</p>
+
+							<Button variant="contained" color="primary" onClick={this.saveChanges}>Save Changes</Button> 
+						</form></>:
 						<><p>Username: {this.props.user.username}</p>
 							<p>What should we call you: {this.props.user.name}</p>
 							<p>Default Color Gradient: {this.props.user.gradient_id}</p>
 							<p>GRADIENT EXAMPLE HERE</p>
-							<Button variant="contained" color="primary" onClick={this.editProfile}>Edit Profile</Button>  </>
-					}</h2>
-						<FormControl variant="outlined">
-							<InputLabel htmlFor="username">
-							Username:</InputLabel>
-						      <TextField
-								    id="outlined-name" label="username"
-									type="username" value={this.state.username}
-								    onChange={this.handleInputChange('username')}
-								    margin="normal" variant="outlined"/>
-							<InputLabel htmlFor="name">
-							Username:</InputLabel>
-								<TextField
-								    id="outlined-name" label="name"
-									type="name" value={this.state.name}
-								    onChange={this.handleInputChange('name')}
-								    margin="normal" variant="outlined"/>
-							<InputLabel htmlFor="gradient_id">
-							Color Gradient</InputLabel>
-					<TextField select
-						variant="filled" label="gradient_id"
-						value={this.state.gradient_id}
-						onChange={this.handleInputChange('gradient_id')}
-						InputProps={{startAdornment: <InputAdornment position="start">gradient</InputAdornment>,
-						}}>
-						{ranges.map(option => (
-							<MenuItem key={option.value} value={option.value}>
-								{option.label}
-							</MenuItem>
-								// <Select value={this.state.gradient_id}
-								// onChange={this.handleInputChange('gradient_id')}
-								// input={<OutlinedInput labelWidth={18} name="gradient_id" id="gradient_id" />}>
-								// <MenuItem value=""><em>None</em></MenuItem>
-								// <MenuItem value={10}>Ten</MenuItem>
-								// <MenuItem value={20}>Twenty</MenuItem>
-								// <MenuItem value={30}>Thirty</MenuItem>
-								// </Select>
-						</FormControl>
-				
+							<Button variant="contained" color="primary" onClick={this.editProfile}>Edit Profile</Button></>
+					}
 			</div>
 		)
 	}
