79ff6763d0fa4fe76d8a2d6f90effb7c42a02922
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-06-06 11:32:23 -0500

    Profile gradient styling, redid some of the rendering

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,6 +16,12 @@ class Profile extends Component {
 		this.props.dispatch({type: 'FETCH_GRADIENT'})
 	}
 
+	cancelEdit = () => {
+		this.setState({
+			profileEditable: false
+		});
+	}
+
 	editProfile = () => {
 		//on edit button click, set profileEditable state to true to trigger conditionally
 		//rendered display to allow profile updates to be made
@@ -23,6 +29,13 @@ class Profile extends Component {
 			profileEditable: true
 		})
 	};//end editProfile
+
+	divClick = (id) => {
+		console.log('div click!', id)
+		this.setState({
+			gradient_id: id
+		})
+	}
 
 	handleInputChange = propertyName => (event) => {
 		this.setState({
@@ -50,39 +63,60 @@ class Profile extends Component {
 				<h2>{this.props.user.username}'s Profile</h2>
 						{/* ternary statement to conditionally render page based on if user is editing profile or not */}
 					    {this.state.profileEditable ?
-						<><form>
-							<label htmlFor="username">
-              				Username:
-              					<input type="text" name="username"
-                				value={this.state.username}
-                				onChange={this.handleInputChange('username')}/></label>
-							<br></br>
-
-							<label htmlFor="gradient_id">
-							Default color gradient:
-								<select value={this.props.user.gradient_id} onChange={this.handleInputChange('gradient_id')}>
-									{this.props.gradient.map((gradient) => {
-										return(
-											<option key={gradient.gradientid} value={gradient.gradientid}>{gradient.name}</option>
-										)
-									})}
-								</select></label>
+						<>
 							{this.props.gradient.map((aGradient) => {
-								return(
-									<div key={aGradient.gradientid}>
-										<p>{aGradient.name}:</p>
-										{aGradient.colors.map((color, i) => {
-											return(
-												<div key={i} className="gradientDiv" style={{backgroundColor: `${color.color}`}}></div>
-											)
-										})}
-									</div>
-								)
-							})}
-							<br></br>
+								if (aGradient.gradientid === this.props.user.gradient_id) {
+									return (
+									<form>
+										<label htmlFor="username">
+		              						Username:
+		              						<input type="text" name="username"
+		                					value={this.state.username}
+		                					onChange={this.handleInputChange('username')}/></label>
+										<br></br>
 
-							<Button variant="contained" color="primary" onClick={this.saveChanges}>Save Changes</Button> 
-						</form>
+										<label htmlFor="gradient_id">
+											Default color gradient: {aGradient.name} -  
+												{aGradient.colors.map((color, i) => {
+													return (
+														<div key={i} className="gradientDiv" style={{backgroundColor: `${color.color}`}}></div>
+													)
+												})}</label>
+										<br></br><br></br>
+
+										<p>Select new color gradient:</p>
+											{this.props.gradient.map((aGradient) => {
+												let id = `${aGradient.gradientid}`
+												return(
+													<div key={aGradient.gradientid} className="selectGradient" 
+														value={this.state.gradient_id} onClick={() => this.divClick(id)}>
+														<p>{aGradient.name}:</p>
+														{aGradient.colors.map((color, i) => {
+															return(
+																<div key={i} className="gradientDiv" style={{backgroundColor: `${color.color}`}}></div>
+															)
+														})}
+													</div>
+												)
+											})}
+											<br></br>
+
+									<Button variant="contained" color="primary" onClick={this.cancelEdit}>Cancel</Button>
+									<Button variant="contained" color="primary" onClick={this.saveChanges}>Save Changes</Button> 
+								</form>
+										// <div key={aGradient.gradientid}>
+										// 	<p>Username: {this.props.user.username}</p>
+										// 	<p>Default Color Gradient: {aGradient.name}</p>
+										// 	{aGradient.colors.map((color, i) => {
+										// 		return (
+										// 			<div key={i} className="gradientDiv" style={{ backgroundColor: `${color.color}` }}></div>
+										// 		)
+										// 	})}
+										// </div>
+									)
+								}
+							})}
+								
 						</>
 						:
 						<>
